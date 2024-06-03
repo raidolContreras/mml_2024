476bc2e87f0a508d61cb13b5cd67f140692b67c1
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2024-06-03 00:44:15 -0600

    languaje

--- view/extras/language.php
+++ view/extras/language.php
@@ -173,7 +173,7 @@ cargarTraducciones('<?php echo $_SESSION['language']; ?>').then(() => {
     $('#level_user_edit').html(html);
     $('.level_user_edit').html(html);
 
-    $('.message_Select_Problems').html(html);
+    $('.message_Select_Problems').text(translations.message_Select_Problems);
 });
 
 function changeLanguage(language) {
